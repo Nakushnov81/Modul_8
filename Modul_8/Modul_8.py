d9b7868a8f7eb9e230cde94d9548a548ccde5763
nakushnov81 <nakushnov@rambler.ru>
2023-05-14 17:52:57 +0400

    Commit

--- Modul_8/Modul_8.py
+++ Modul_8/Modul_8.py
@@ -1,11 +1,13 @@
 import sqlite3
-from datetime import datetime
+# from datetime import datetime
 conn = sqlite3.connect('Varsity.sqlite')
 cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS Student (id INTEGER PRIMARY KEY, name Varchar(32), surname Varchar(32), age int, city Varchar(32);')
-cur.execute('CREATE TABLE IF NOT EXISTS Course (id INTEGER PRIMARY KEY, time_start TIMESTAMP, time_end TIMESTAMP);')
-cur.execute('CREATE TABLE IF NOT EXISTS Student_courses (FOREIGN_KEY(student_id) REFERENCE Student(id),'
-            ' FOREIGN_KEY(course_id) REFERENCE Course(id);')
+cur.execute('''CREATE TABLE IF NOT EXISTS Student (id INT PRIMARY KEY, name TEXT, surname TEXT, age int, city TEXT)''')
+
+cur.execute('CREATE TABLE IF NOT EXISTS Course (id INT PRIMARY KEY, name TEXT, time_start TIMESTAMP, time_end TIMESTAMP)')
+
+cur.execute('CREATE TABLE IF NOT EXISTS Student_courses (student_id INT, FOREIGN KEY(student_id) REFERENCES Student(id), course_id INT, FOREIGN KEY(course_id) REFERENCES Course(id))')
+
 stud = [
     (1, 'Max', 'Brooks', 24, 'Spb'),
     (2, 'John', 'Stones', 15, 'Spb'),
@@ -18,12 +20,21 @@ courses = [(1, 'python', '21.07.21', '21.08.21'),
 
 Student_courses = [(1, 1), (2, 1), (3, 1), (4, 2)]
 
-cur.executemany('INSERT INTO Student VALUES(?,?,?,?,?)', stud)
+# cur.executemany("""INSERT INTO Student VALUES(?,?,?,?,?)""", stud)
 
-cur.executemany('INSERT INTO Course VALUES(?,?,?,?)', courses)
-
-cur.executemany('INSERT INTO Course VALUES(?,?)', Student_courses)
-
+# cur.executemany("""INSERT INTO Course VALUES(?,?,?,?)""", courses)
+# conn.commit()
+# cur.executemany('''INSERT INTO Course VALUES(?,?)''', Student_courses)
+# cur.execute("DROP TABLE Student_courses")
 conn.commit()
-# conn.close()
+
+# cur.execute("""SELECT * FROM Course""")
+
+# print(cur.fetchall())
+
+cur.execute("""SELECT * FROM Student_courses""")
+
+print(cur.fetchall())
+
+conn.close()
 
